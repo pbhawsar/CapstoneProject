5356e714673ffe431cd7f5f35fc8e823eb427513
Pranay Bhawsar <bhawsar@adobe.com>
2024-01-26 11:19:51 -0800

    insure-me app source code along with dockerfile

---
+++ src/main/java/com/project/insurance/insureme/InsureMeApplication.java
@@ -0,0 +1,13 @@
+package com.project.insurance.insureme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsureMeApplication.class, args);
+	}
+
+}
